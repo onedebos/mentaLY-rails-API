baa9cb59c026f58e4a82cabd5d397fdc40d69e86
Adebola <onedebos@gmail.com>
2020-02-09 23:17:50 +0100

    update readme and providers logos

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ Provider.create!(
     name:'Mentally Aware',
     email:'contact@mentallyaware.org',
     state:'Lagos',
-    logo:'logo.clearbit.com/mentalup.net',
+    logo:'https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png',
     description: 'We are focused on ending mental health stigma, and creating a comfortable environment for open conversations about mental health issues. As an organization, we have come to understand the importance of stories in challenging inbred notions about mental health issues, especially when the stories come from those with lived experience. This is what our campaigns revolve around and we can already see the change in attitude that has occurred as a result of this.'
 )
 
@@ -19,7 +19,7 @@ Provider.create!(
     name:'MyPaddi',
     email:'admin@mypadding.com',
     state:'Ondo',
-    logo:'logo.clearbit.com/mentalup.net',
+    logo:'https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png',
     description: 'Online community of young people accessing sexual & reproductive health information & products anonymously'
 )
 
@@ -27,7 +27,7 @@ Provider.create!(
     name:'Mental Health Foundation',
     email:'owoyemi@mentalhealthnigeria.org',
     state:'Ekiti',
-    logo:'logo.clearbit.com/mentalup.net',
+    logo:'https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png',
     description: 'Mental Health Foundation works assiduously to help all people prevent and overcome mental disorders, providing adequate information, education, encouragement, and support for people living with either mild or severe mental disorders alongside their family member.'
 )
 
@@ -35,7 +35,7 @@ Provider.create!(
     name:'Stand to End Rape(STER)',
     email:'STERAbuja@standtoendrape.org',
     state:'Abuja',
-    logo:'logo.clearbit.com/mentalup.net',
+    logo:'https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png',
     description: 'We are a youth-led Not-for-Profit Organization advocating against sexual violence, providing prevention mechanisms and supporting survivors with psychosocial services. We advocate for rape survivors who can’t speak about their ordeal due to stigmatization, by enlighten our community on the need to end rape and victim blaming.'
 )
 
@@ -43,7 +43,7 @@ Provider.create!(
     name:'Aro Neuropsychiatric Hospital',
     email:'aro@aro.ng',
     state:'Ogun',
-    logo:'logo.clearbit.com/mentalup.net',
+    logo:'https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png',
     description: 'A federal institution (hospital) where mentally ill patients are cared for without attaching any stigma and it is rich with historical legacy since inception.'
 )
 
@@ -51,7 +51,7 @@ Provider.create!(
     name:'PsychNG Services',
     email:'book@psychng.com',
     state:'Lagos',
-    logo:'logo.clearbit.com/mentalup.net',
+    logo:'https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png',
     description: 'PsychNG Services provides psychological services for affective disorders; depression, anxiety and other related mood disorders. Our website also provide guided Audio lessons for stress relief and more! We understand that, sometimes talking about mental health can get a bit heavy - hence, the reason for the birth of PsychNG Community Forum. It is the place to be if you want to chill out and socialize with other members on similar topics. Our mini social network page encourages easy communication with friends and professionals. Share your stories. Share your journey. Help someone.'
 )
 
