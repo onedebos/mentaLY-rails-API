f815d044ce20ca36b31a6381df4d023d7127b7fb
Adebola <onedebos@gmail.com>
2020-02-10 22:35:03 +0100

    allow all access

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,6 +8,14 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
+    origins "*"
+    resource "*", headers: :any, methods: [
+        :get, :post, :put, :patch, :delete, :options, :head
+    ],
+    credentials: true
+  end
+
+  allow do
     origins "https://mentallly.herokuapp.com"
     resource "*", headers: :any, methods: [
         :get, :post, :put, :patch, :delete, :options, :head
