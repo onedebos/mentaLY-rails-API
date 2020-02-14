3a86fbdfe913c9c2d20e86f2448acfc8cbbd21ba
Adebola <onedebos@gmail.com>
2020-02-14 16:42:27 +0100

    fix lint issues

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -1,10 +1,10 @@
 module
   CurrentUserConcern extend ActiveSupport::Concern
-  included do
-    before_action :set_current_user
-  end
+                     included do
+                       before_action :set_current_user
+                     end
 
-  def set_current_user
-    return @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
+                     def set_current_user
+                       return @current_user ||= User.find(session[:user_id]) if session[:user_id]
+                     end
 end
