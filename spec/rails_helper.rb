175249790b30cec00816e66b06a37da60de6cbee
Adebola <onedebos@gmail.com>
2020-02-15 15:22:59 +0100

    fix lint errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,8 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../../config/environment', __dir__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
@@ -70,7 +70,6 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.include RequestSpecHelper
-  
   # Filter lines from Rails gems in backtraces.
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
