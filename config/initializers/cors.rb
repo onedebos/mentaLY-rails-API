7a19072c10eb0ac2b1bca774bd193108eff8ac31
Adebola <onedebos@gmail.com>
2020-05-18 22:35:21 +0100

    update initializer file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -25,6 +25,14 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
   
   allow do
+    origins "https://awesome-kare-c984ee.netlify.app"
+    resource "*", headers: :any, methods: [
+        :get, :post, :put, :patch, :delete, :options, :head
+    ],
+    credentials: true
+  end
+
+  allow do
     origins "https://awesome-kare-c984ee.netlify.com/login"
     resource "*", headers: :any, methods: [
         :get, :post, :put, :patch, :delete, :options, :head
